c0c27fe15cb998986839427a4be9bf190d3a21af
StoverS <stovers@sou.edu>
2020-10-14 00:30:08 -0700

    added blastoff.c

---
+++ blastoff.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+        const int start = 10;
+        for (int i = 0; i<=start; i++) {
+                printf("%d", i);
+                printf("\n"); }
+        printf("BLAST OFF\n");
+        return 0;
+}
